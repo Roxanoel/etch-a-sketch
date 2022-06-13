bb8f1ff95772c27ac3d8702fcd44ea4a2b5acd37
Roxanoel <roxane.noel92@gmail.com>
2022-06-13 13:58:48 +0100

    Implemented "create new grid" (prompts user for new grid size, flushes previous grid, creates new grid with the specified dimensions).

--- script.js
+++ script.js
@@ -1,32 +1,54 @@
 // config params
-let gridSize = 16;
+const defaultGridSize = 16;
+const maxSize = 100;
 
 // Cached refs
 const container = document.querySelector('.grid-container');
 const squares = [];
+const button = document.querySelector('button');
 
-generateGrid();
+// Main
+button.addEventListener('click', setupNewGrid)
 
-function generateGrid() {
+generateGrid(defaultGridSize);
+
+// Function definitions 
+
+function generateGrid(size) {
+    // Deleting any pre-existing squares
+    removeAllChildren(container);
+
     // Setting the grid size to the correct size
-    container.style.gridTemplateColumns = `repeat(${gridSize}, 1fr)`;
+    container.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
     
     // creating each square and appending it in the same go
-    for (let i = 0; i < gridSize*gridSize; i++) {
+    for (let i = 0; i < size*size; i++) {
         const newDiv = document.createElement('div');
         newDiv.classList.add('pixel');
         container.appendChild(newDiv);
         squares.push(newDiv);
     }
 
-    addListeners(squares);
+    addHoverListeners(squares);
 }
 
-function addListeners(array) {
+function removeAllChildren(parent) {
+    while (parent.firstChild) {
+        parent.removeChild(parent.firstChild);
+    }
+}
+
+function addHoverListeners(array) {
     array.forEach(element => element.addEventListener('mouseenter', 
     element => element.target.classList.add('coloured')));
 }
 
-function makeColoured(element) {
-    
+function setupNewGrid() {
+    const newSize = +(prompt(`How wide should the new grid be? Maximum size = ${maxSize}`));
+    if (newSize <= maxSize){
+        generateGrid(newSize);
+    }
+    else{
+        alert('This is not a valid number, please try again.');
+    }
 }
